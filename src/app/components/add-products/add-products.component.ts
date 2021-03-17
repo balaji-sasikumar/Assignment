7fce4da86e78fecc64e7532e0a7dc8714929220c
Balaji Sasikumar <balaji17023@cse.ssn.edu.in>
2021-03-17 18:34:31 +0530

    UI v1

--- src/app/components/add-products/add-products.component.ts
+++ src/app/components/add-products/add-products.component.ts
@@ -3,90 +3,96 @@ import { ProductsService } from '../../services/products.service';
 import Swal from 'sweetalert2';
 import { ActivatedRoute } from '@angular/router';
 
-
-
 @Component({
   selector: 'app-add-products',
   templateUrl: './add-products.component.html',
-  styleUrls: ['./add-products.component.css']
+  styleUrls: ['./add-products.component.css'],
 })
 export class AddProductsComponent implements OnInit {
-  isUpdate=false;
-  id="";listProducts;product;
+  isUpdate = false;
+  id = '';
+  listProducts;
+  product;
   constructor(
-    private productsService:ProductsService,private activatedRoute: ActivatedRoute
-  ) {
-   }
-  formValue={
-    productName:"",
-    availableQuantity:1,
-    price:0
-  }
-   sliceInput(max) {
-    if (String(this.formValue.availableQuantity).length > max){
-      this.formValue.availableQuantity = Number(String(this.formValue.availableQuantity).slice(0, max));
-      Swal.fire(" ", "Please provide valid input", 'error');
+    private productsService: ProductsService,
+    private activatedRoute: ActivatedRoute
+  ) {}
+  formValue = {
+    productName: '',
+    availableQuantity: 1,
+    price: 1,
+  };
+  sliceInput(data,max) {
+    if (String(data).length > max) {
+      data = Number(
+        String(data).slice(0, max)
+      );
+      Swal.fire(' ', 'Please provide valid input', 'error');
+      this.formValue = {
+        productName: '',
+        availableQuantity: 1,
+        price: 1,
+      };
     }
-    
   }
 
-  onSubmit(formValue){
-    if(!this.isUpdate){
-      this.productsService.addProduct(formValue)
-      .subscribe(
-        (result)=>{
-          Swal.fire(" ","Product Added",'success');
-          console.log(result)
-          // location.reload()
-        },
-        (error)=>{
-          Swal.fire(" ","Please check the input field",'error')//exception use
-        }
-      )
+  onSubmit(formValue) {
+    if(this.formValue.productName!==''){
+      if (!this.isUpdate) {
+        this.productsService.addProduct(formValue).subscribe(
+          (result) => {
+            Swal.fire(' ', 'Product Added', 'success');
+            // console.log(result);
+            // location.reload()
+          },
+          (error) => {
+            Swal.fire(' ', 'Please check the input field', 'error'); //exception use
+          }
+        );
+      } else {
+        this.productsService.updateProduct(this.id, formValue).subscribe(
+          (result) => {
+            Swal.fire(' ', 'Product Updated', 'success');
+            // console.log(result);
+            // location.reload()
+          },
+          (error) => {
+            Swal.fire(' ', 'Please check the input field ', 'error'); //exception use
+          }
+        );
+      }
     }
     else{
-      this.productsService.updateProduct(this.id,formValue)
-      .subscribe(
-        (result)=>{
-          Swal.fire(" ","Product Updated",'success');
-          console.log(result)
-          // location.reload()
-        },
-        (error)=>{
-          Swal.fire(" ","Please check the input field ",'error')//exception use
-        }
-      )
+      Swal.fire("","Please provide data to the input field","error")
     }
-    this.formValue={
-      productName:"",
-      availableQuantity:1,
-      price:0
-    }
-
+    this.formValue = {
+      productName: '',
+      availableQuantity: 1,
+      price: 1,
+    };
   }
 
-  onBack(){
-    Swal.fire("Are you sure,Your entry will be deleted");
-    
+  onBack() {
+    Swal.fire('Are you sure,Your entry will be deleted');
   }
 
   ngOnInit(): void {
     this.id = this.activatedRoute.snapshot.params.id;
-    if(this.id){
-      this.isUpdate=true;
-      console.log(this.id)
+    if (this.id) {
+      this.isUpdate = true;
+      // console.log(this.id);
       this.productsService.getData();
-      this.productsService.productList.subscribe(productList=>{
+      this.productsService.productList.subscribe((productList) => {
         this.listProducts = productList;
-        this.product = this.listProducts.filter(product => product.productId === this.id);
-        this.formValue={
-          productName:this.product[0].productName,
-          availableQuantity:this.product[0].availableQuantity,
-          price:this.product[0].price
-        }
-      })
+        this.product = this.listProducts.filter(
+          (product) => product.productId === this.id
+        );
+        this.formValue = {
+          productName: this.product[0].productName,
+          availableQuantity: this.product[0].availableQuantity,
+          price: this.product[0].price,
+        };
+      });
     }
-
   }
-
 }
