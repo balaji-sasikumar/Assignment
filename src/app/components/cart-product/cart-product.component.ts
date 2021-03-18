2d56e3c143ac7b836d630b2d857f7253d964930d
Balaji Sasikumar <balaji17023@cse.ssn.edu.in>
2021-03-18 17:20:27 +0530

    sort implemented

--- src/app/components/cart-product/cart-product.component.ts
+++ src/app/components/cart-product/cart-product.component.ts
@@ -1,37 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
 import { Orders } from 'src/app/models/Model';
 import { OrdersService } from '../../services/orders.service';
 import Swal from 'sweetalert2';
-
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-cart-product',
   templateUrl: './cart-product.component.html',
   styleUrls: ['./cart-product.component.css'],
 })
-export class CartProductComponent implements OnInit {
-  orderList: Orders[];
+export class CartProductComponent implements OnInit, AfterViewInit {
+  orderList;
   format = 'MMM d,y hh:mm a';
-  constructor(private ordersService: OrdersService) {}
-  getData() {
+  displayedColumns: string[] = [
+    'number',
+    'productName',
+    'quantity',
+    'invoiceDate',
+    'deliveredDate',
+    'totalPrice',
+    'action',
+  ];
+  // columnDefs = [
+  //   { headerName:'Number',field: 'number' },
+  //   { headerName:'Prdouct Name',field: 'product.productName' },
+  //   { headerName:'Quantity',field: 'quantity' },
+  //   { headerName:'Invoice Date',field: 'invoiceDate' },
+  //   { headerName:'Delivered Date',field: 'deliveredDate' },
+  //   { headerName:'Total Price',field: 'quantity * product.price ' },
+  //   { headerName:'Action',field: 'action' },
+  // ];
+  constructor(private ordersService: OrdersService) {
     this.ordersService.getData();
-    this.ordersService.orderList.subscribe((response) => {
+    this.ordersService.orderList.subscribe((response: Orders[]) => {
       // console.log(response);
-      this.orderList = response;
+      this.orderList = new MatTableDataSource(response);
     });
+  }
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  ngOnInit(): void {
+    this.ngAfterViewInit();
+  }
+  ngAfterViewInit() {
+    this.orderList.sort = this.sort;
   }
   deleteData(id) {
     this.ordersService.deleteProduct(id).subscribe(
       (response) => {
+        console.log(response);
+        console.log(id);
         Swal.fire(' ', 'Order Deleted', 'success');
-        this.ngOnInit();
+        setTimeout(() => {
+          location.reload();
+        }, 3000);
       },
       (error) => {
+        console.log(error);
         Swal.fire(' ', 'Order cannot be deleted', 'error');
       }
     );
-  }
-
-  ngOnInit(): void {
-    this.getData();
   }
 }
--- src/assets/packages/material.ts
+++ src/assets/packages/material.ts
@@ -1,14 +1,9 @@
-import { NgModule } from "@angular/core";
-import {MatTableModule} from "@angular/material/table";
-import { MatSortModule  } from "@angular/material/sort";
-
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatSortModule } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
 @NgModule({
-        exports:[
-                MatTableModule,MatSortModule
-        ],
-        providers:[
-
-        ]
+  exports: [MatTableModule, MatSortModule, MatIconModule],
+  providers: [],
 })
-
-export class Material{}
+export class Material {}
